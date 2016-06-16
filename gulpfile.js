dcb8924cacf8c3e7c86a67bcc10536366119f1b3
wsw <wsw0108@gmail.com>
2016-06-16 11:18:05 +0800

    add 'order' to file's metadata

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,59 @@ var metalsmith = require('gulp-metalsmith');
 var layouts = require('metalsmith-layouts');
 var drafts = require('metalsmith-drafts');
 
+var markupRegex = /([^\/^\.]*)\.html$/;
+
+var exampleInfo = require('./examples/examples.json');
+var orderedKeys = [];
+for (var i = 0; i < exampleInfo.examples.length; i++) {
+  var dir = exampleInfo.examples[i][0];
+  var subdirs = exampleInfo.examples[i][2];
+  for (var j = 0; j < subdirs.length; j++) {
+    var subdir = subdirs[j];
+    orderedKeys.push(path.join(dir, subdir));
+  }
+}
+
+function processSingleFile (file, filepath, files) {
+  var basename = path.basename(filepath);
+  var match = basename.match(markupRegex);
+  if (!match) return;
+
+  var id = match[1];
+  var dirname = path.dirname(filepath);
+
+  var js = path.join(dirname, id + '.js');
+  if (js in files) {
+    file.js = {
+      source: files[js].contents.toString()
+    };
+  }
+
+  var css = path.join(dirname, id + '.css');
+  if (css in files) {
+    file.css = {
+      source: files[css].contents.toString()
+    };
+  }
+
+  file.order = orderedKeys.indexOf(dirname);
+}
+
 function processRaw (files, metalsmith, done) {
   setImmediate(done);
-  var order = 1;
-  // TODO: get order from examples.json
-  for (var filename in files) {
-    var file = files[filename];
-    file.order = order;
-    order++;
+  for (var filepath in files) {
+    var file = files[filepath];
+    processSingleFile(file, filepath, files);
   }
 }
 
 function processDemo (files, metalsmith, done) {
   setImmediate(done);
-  var order = 1;
-  for (var filename in files) {
-    var file = files[filename];
-    file.basename = path.basename(filename);
+  for (var filepath in files) {
+    var file = files[filepath];
+    var basename = path.basename(filepath);
+    processSingleFile(file, filepath, files);
+    file.basename = basename;
   }
 }
 
