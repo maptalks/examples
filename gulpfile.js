32c120c880eadbec62be8207bbef9caabbcf22cf
wangjun <wangjun@maptalks.org>
2016-06-22 15:01:02 +0800

    add scale demo.

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ var defines = define({
       'subdomains': '[1, 2, 3, 4]'
     },
     'en': {
-      'urlTemplate': 'http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}',
+      //'urlTemplate': 'http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}',
+      'urlTemplate' : 'http://webrd0{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}',
       'subdomains': '[1, 2, 3, 4]'
     }
   }
