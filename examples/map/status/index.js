537052ac3ef971b83cce0606c314328c4f6d1ebf
FuZhenn <fuzhen@maptalks.org>
2016-06-22 16:18:46 +0800

    update and add animation

--- examples/map/status/index.js
+++ examples/map/status/index.js
@@ -1,15 +1,15 @@
 
-var map = new maptalks.Map("map", {
+var map = new maptalks.Map('map', {
   center: [121.48542888885189, 31.228541533313702],
   zoom: 14,
-  baseLayer: new maptalks.TileLayer("base", {
+  baseLayer: new maptalks.TileLayer('base', {
     urlTemplate: '$(urlTemplate)',
     subdomains: $(subdomains)
   })
 });
 
 var extent = map.getExtent(),
-    ex = [
+    info = [
       '{',
       '  xmin:' + extent.xmin,
       '  ymin:' + extent.ymin,
@@ -20,7 +20,7 @@ var extent = map.getExtent(),
 
 var mapStatus = [
     'Center : ' + '\n[' + map.getCenter().toArray().join() + ']',
-    'Extent : ' + '\n' + ex,
+    'Extent : ' + '\n' + info,
     'Size :' + map.getSize().toArray().join(),
     'Zoom : '   + map.getZoom(),
     'MinZoom : ' + map.getMinZoom(),
@@ -28,4 +28,6 @@ var mapStatus = [
     'Projection : ' + map.getProjection().code
 ];
 
-document.getElementById('status').innerHTML = '<pre>' + mapStatus.join('\n') + '</pre>';
+var content = '<pre>' + mapStatus.join('\n') + '</pre>';
+
+document.getElementById('status').innerHTML = content;
