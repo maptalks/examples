8ab8be316788912374ff2207e27d091416e65c59
wangjun <wangjun@maptalks.org>
2016-06-26 15:39:33 +0800

    Add custom menu demo.

---
+++ examples/ui/custom-menu/index.js
@@ -0,0 +1,20 @@
+
+var map = new maptalks.Map("map", {
+  center: [121.48542888885189, 31.228541533313702],
+  zoom: 14,
+  baseLayer: new maptalks.TileLayer("tile", {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains)
+  })
+});
+
+var options = {
+    'custom': true,
+    'items'  : '<div class="custom_menu"><span onclick="clickItem(this);">item1</span><br/>'
+              +'<span onclick="clickItem(this);">item2</span></div>'
+};
+map.setMenu(options).openMenu();
+
+function clickItem(dom) {
+  alert('Click ' + dom.innerText);
+}
