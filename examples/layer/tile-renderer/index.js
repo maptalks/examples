6f1d962dd3c1d6d31c73ef098f070a2b7c2605f2
wsw <wsw0108@gmail.com>
2016-06-26 18:43:23 +0800

    update 'tile-renderer'

--- examples/layer/tile-renderer/index.js
+++ examples/layer/tile-renderer/index.js
@@ -9,6 +9,10 @@ var map1 = new maptalks.Map('map-on-dom', {
   })
 });
 
+$('#map-on-canvas')
+  .width($('#map-on-dom').width())
+  .height($('#map-on-dom').height());
+
 var map2 = new maptalks.Map('map-on-canvas', {
   center: [121.48542888885189, 31.228541533313702],
   zoom: 14,
