636d20a18eb0e629e39a2db2eec34fcf6cdf6691
wangjun <wangjun@maptalks.org>
2016-06-27 23:26:04 +0800

    some update.

--- examples/ui/custom-ui/index.js
+++ examples/ui/custom-ui/index.js
@@ -17,7 +17,7 @@ var MyUI = maptalks.ui.UIComponent.extend({
 var map = new maptalks.Map("map", {
   center: [121.48542888885189, 31.228541533313702],
   zoom: 14,
-  baseLayer: new maptalks.TileLayer("tile", {
+  baseLayer: new maptalks.TileLayer("base", {
     urlTemplate: '$(urlTemplate)',
     subdomains: $(subdomains)
   })
