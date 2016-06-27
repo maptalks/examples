ba5d7b2cb04d8e82fdc7feda331b07877b667e25
wangjun <wangjun@maptalks.org>
2016-06-27 11:16:02 +0800

    Add file marker demo.

---
+++ examples/style/file-marker/index.js
@@ -0,0 +1,14 @@
+
+var map = new maptalks.Map("map", {
+  center: [121.48542888885189, 31.228541533313702],
+  zoom: 14,
+  baseLayer: new maptalks.TileLayer("tile", {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains)
+  })
+});
+
+var layer = new maptalks.VectorLayer('vector');
+map.addLayer(layer);
+var geometry = new maptalks.Marker([121.485428, 31.228541]);
+layer.addGeometry(geometry);
