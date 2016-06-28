732321958223575ce2e14a7c9c6dd6dd66d86937
wangjun <wangjun@maptalks.org>
2016-06-28 16:53:38 +0800

    Add vector marker demo.

--- examples/style/vector-marker/index.js
+++ examples/style/vector-marker/index.js
@@ -11,10 +11,16 @@ var map = new maptalks.Map('map', {
 var layer = new maptalks.VectorLayer('vector');
 map.addLayer(layer);
 var symbol = {
-    'textName'          : 'TEXT MARKER',
-    'textFaceName'      : 'monospace',
-    'textSize'          : 12,
-    'textFill'          : '#6fa8dc'
-};
+    'markerType': 'ellipse',
+    'markerFill': '#6fa8dc',
+    'markerFillOpacity': 1,
+    'markerLineColor': '#f9cb9c',
+    'markerLineWidth': 1,
+    'markerLineOpacity': 1,
+    'markerWidth': 30,
+    'markerHeight': 30,
+    'markerDx': 0,
+    'markerDy': 0
+ };
 var geometry = new maptalks.Marker([121.485428, 31.228541], {'symbol' : symbol});
 layer.addGeometry(geometry);
