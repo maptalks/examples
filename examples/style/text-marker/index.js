3cf01371422042668cd08e933f4fcfceac7659a3
wangjun <wangjun@maptalks.org>
2016-06-28 16:35:52 +0800

    Add text marker demo.

--- examples/style/text-marker/index.js
+++ examples/style/text-marker/index.js
@@ -11,9 +11,17 @@ var map = new maptalks.Map('map', {
 var layer = new maptalks.VectorLayer('vector');
 map.addLayer(layer);
 var symbol = {
-    'markerFile'   : '../../../../../images/style/marker.png',
-    'markerWidth'  : 32,
-    'markerHeight' : 32,
+    'textName'          : 'TEXT MARKER',
+    'textFaceName'      : 'monospace',
+    'textSize'          : 12,
+    'textFill'          : '#6fa8dc',
+    'textOpacity'       : 1,
+    'textHaloFill'      : '#ffffff',
+    'textHaloRadius'    : 0,
+    'textLineSpacing'   : 0,
+    'textHorizontalAlignment' : 'middle',
+    'textVerticalAlignment'   : 'middle',
+    'textAlign'               : 'center'
 };
 var geometry = new maptalks.Marker([121.485428, 31.228541], {'symbol' : symbol});
 layer.addGeometry(geometry);
