7fc1fbd9f20e639551d3f188e62a1dd4e550b5e2
FuZhenn <fuzhen@maptalks.org>
2016-07-04 10:18:30 +0800

    fix lint

--- examples/ui/highcharts-ui/index.js
+++ examples/ui/highcharts-ui/index.js
@@ -13,67 +13,67 @@ barDom.style.cssText = 'min-width: 300px; height: 300px; margin: 0 auto;';
 createBar(barDom);
 
 function createBar(dom) {
- new Highcharts.Chart({
-     chart: {
-          renderTo: dom,
-          backgroundColor: 'rgba(255, 255, 255, 1)',
-          type: 'area',
-          spacingBottom: 30
-      },
+  new Highcharts.Chart({
+    chart: {
+      renderTo: dom,
+      backgroundColor: 'rgba(255, 255, 255, 1)',
+      type: 'area',
+      spacingBottom: 30
+    },
+    title: {
+      text: 'Fruit consumption *'
+    },
+    subtitle: {
+      text: '* Jane\'s banana consumption is unknown',
+      floating: true,
+      align: 'right',
+      verticalAlign: 'bottom',
+      y: 15
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 150,
+      y: 100,
+      floating: true,
+      borderWidth: 1,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    xAxis: {
+      categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
+    },
+    yAxis: {
       title: {
-          text: 'Fruit consumption *'
+        text: 'Y-Axis'
       },
-      subtitle: {
-          text: '* Jane\'s banana consumption is unknown',
-          floating: true,
-          align: 'right',
-          verticalAlign: 'bottom',
-          y: 15
-      },
-      legend: {
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          x: 150,
-          y: 100,
-          floating: true,
-          borderWidth: 1,
-          backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
-      },
-      xAxis: {
-          categories: ['Apples', 'Pears', 'Oranges', 'Bananas', 'Grapes', 'Plums', 'Strawberries', 'Raspberries']
-      },
-      yAxis: {
-          title: {
-              text: 'Y-Axis'
-          },
-          labels: {
-              formatter: function () {
-                  return this.value;
-              }
-          }
-      },
-      tooltip: {
-          formatter: function () {
-              return '<b>' + this.series.name + '</b><br/>' +
+      labels: {
+        formatter: function () {
+          return this.value;
+        }
+      }
+    },
+    tooltip: {
+      formatter: function () {
+        return '<b>' + this.series.name + '</b><br/>' +
                   this.x + ': ' + this.y;
-          }
-      },
-      plotOptions: {
-          area: {
-              fillOpacity: 0.5
-          }
-      },
-      credits: {
-          enabled: false
-      },
-      series: [{
-          name: 'John',
-          data: [0, 1, 4, 4, 5, 2, 3, 7]
-      }, {
-          name: 'Jane',
-          data: [1, 0, 3, null, 3, 1, 2, 1]
-      }]
+      }
+    },
+    plotOptions: {
+      area: {
+        fillOpacity: 0.5
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      name: 'John',
+      data: [0, 1, 4, 4, 5, 2, 3, 7]
+    }, {
+      name: 'Jane',
+      data: [1, 0, 3, null, 3, 1, 2, 1]
+    }]
     /*chart: {
         renderTo: dom,
         backgroundColor: 'rgba(255, 255, 255, 0)',
