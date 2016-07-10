c608610435ccf8932f8c5d3b8aa000a56b3150b0
FuZhenn <fuzhen@maptalks.org>
2016-07-10 14:59:47 +0800

    update exmple.hbs and update status example

--- examples/map/status/index.js
+++ examples/map/status/index.js
@@ -8,26 +8,31 @@ var map = new maptalks.Map('map', {
   })
 });
 
-var extent = map.getExtent(),
-  ex = [
-    '{',
-    '  xmin:' + extent.xmin,
-    '  ymin:' + extent.ymin,
-    '  xmax:' + extent.xmax,
-    '  ymax:' + extent.xmax,
-    '}'
-  ].join('\n');
+map.on('zoomend moving moveend', getStatus);
 
-var mapStatus = [
-  'Center : \n[' + map.getCenter().toArray().join() + ']',
-  'Extent : \n' + ex,
-  'Size :' + map.getSize().toArray().join(),
-  'Zoom : '   + map.getZoom(),
-  'MinZoom : ' + map.getMinZoom(),
-  'MaxZoom : ' + map.getMaxZoom(),
-  'Projection : ' + map.getProjection().code
-];
+getStatus();
 
+function getStatus() {
+  var extent = map.getExtent(),
+    ex = [
+      '{',
+      'xmin:' + extent.xmin.toFixed(5),
+      ', ymin:' + extent.ymin.toFixed(5),
+      ', xmax:' + extent.xmax.toFixed(5),
+      ', ymax:' + extent.xmax.toFixed(5),
+      '}'
+    ].join('');
+  var center = map.getCenter();
+  var mapStatus = [
+    'Center : [' + [center.x.toFixed(5), center.y.toFixed(5)].join() + ']',
+    'Extent : ' + ex,
+    'Size : ' + map.getSize().toArray().join(),
+    'Zoom : '   + map.getZoom(),
+    'MinZoom : ' + map.getMinZoom(),
+    'MaxZoom : ' + map.getMaxZoom(),
+    'Projection : ' + map.getProjection().code
+  ];
 
-var content = '<pre>' + mapStatus.join('\n') + '</pre>';
-document.getElementById('status').innerHTML = content;
+  document.getElementById('status').innerHTML = '<pre>' + mapStatus.join('\n') + '</pre>';
+}
+
