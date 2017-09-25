a4d9d51cb98670068bbc3e12101fd11f7ba7142c
fuzhenn <fuzhen@maptalks.org>
2017-09-26 01:24:47 +0800

    update map's container class

--- src/map/sync-map/index.js
+++ src/map/sync-map/index.js
@@ -4,10 +4,43 @@ var map0 = new maptalks.Map('map0', {
   attribution: {
     content: '$(attribution)'
   },
+  zoomControl : true,
   baseLayer: new maptalks.TileLayer('base', {
     urlTemplate: '$(urlTemplate)',
     subdomains: $(subdomains)
   })
+});
+
+
+var map1 = new maptalks.Map('map1', {
+  center: map0.getCenter(),
+  zoom: map0.getZoom(),
+  draggable : false,        // disable draggble
+  scrollWheelZoom : false,  // disable scroll wheel zoom
+  dblClickZoom : false,     // disable doubleclick
+  attribution: {
+    content: '$(attribution)'
+  },
+  baseLayer: new maptalks.TileLayer('base1', {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains)
+  })
+});
+
+map0.on('moving', function (e) {
+  map1.setCenter(e.target.getCenter());
+});
+
+map0.on('zoomend', function (e) {
+  map1.setCenterAndZoom(e.target.getCenter(), e.target.getZoom());
+});
+
+map0.on('pitch', function (e) {
+  map1.setPitch(e.target.getPitch());
+});
+
+map0.on('rotate', function (e) {
+  map1.setBearing(e.target.getBearing());
 });
 
 new maptalks.control.Toolbar({
@@ -18,26 +51,3 @@ new maptalks.control.Toolbar({
   }]
 })
 .addTo(map0);
-
-var map1 = new maptalks.Map('map1', {
-  center: [-0.113049,51.498568],
-  zoom: 14,
-  attribution: {
-    content: '$(attribution)'
-  },
-  draggable : false,
-  scrollWheelZoom : false,
-  dblClickZoom : false,
-  baseLayer: new maptalks.TileLayer('base1', {
-    urlTemplate: '$(urlTemplate)',
-    subdomains: $(subdomains)
-  })
-});
-
-map0.on('moving', function (param) {
-  map1.setCenter(param.target.getCenter());
-});
-
-map0.on('zoomend', function (param) {
-  map1.setCenterAndZoom(param.target.getCenter(), param.target.getZoom());
-});
