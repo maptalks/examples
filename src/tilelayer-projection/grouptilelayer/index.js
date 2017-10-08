687670ab0b9482536c631f574942d14e46c3003c
fuzhenn <fuzhen@maptalks.org>
2017-10-08 12:00:13 +0800

    updates

--- src/tilelayer-projection/grouptilelayer/index.js
+++ src/tilelayer-projection/grouptilelayer/index.js
@@ -7,9 +7,15 @@ var map = new maptalks.Map('map', {
   },
   // add 2 TileLayers with a GroupTileLayer
   baseLayer : new maptalks.GroupTileLayer('base', [
-    new maptalks.TileLayer('tile1',{
-      urlTemplate: 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
-      'subdomains': ['a','b']
+    new maptalks.WMSTileLayer('wms', {
+      'urlTemplate' : 'https://demo.boundlessgeo.com/geoserver/ows',
+      'crs' : 'EPSG:3857',
+      'layers' : 'ne:ne',
+      'styles' : '',
+      'version' : '1.3.0',
+      'format': 'image/png',
+      'transparent' : true,
+      'uppercase' : true
     }),
     new maptalks.TileLayer('tile2',{
       urlTemplate: 'http://korona.geog.uni-heidelberg.de/tiles/adminb/x={x}&y={y}&z={z}'
