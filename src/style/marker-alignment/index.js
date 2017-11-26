8e4cc45fa3564ad2190a2ec5b49cdcce8fc6e5df
fuzhenn <fuzhen@maptalks.org>
2017-11-27 00:59:29 +0800

    add marker-alignment example

---
+++ src/style/marker-alignment/index.js
@@ -0,0 +1,41 @@
+var map = new maptalks.Map('map', {
+  center: [-0.113049,51.49856],
+  zoom: 14,
+  attribution: {
+    content: '$(attribution)'
+  },
+  baseLayer: new maptalks.TileLayer('base', {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains)
+  })
+});
+
+var center = map.getCenter();
+
+var layer = new maptalks.VectorLayer('vector').addTo(map);
+
+var marker1 = new maptalks.Marker(
+  center.add(0.01, 0),
+  {
+    'symbol' : {
+      'textName' : 'm4',
+      'textSize' : 14,
+      'markerFile'   : 'm4.png',
+      'markerHorizontalAlignment' : 'middle', // left, middle(default), right
+      'markerVerticalAlignment' : 'middle'    // top, middle, bottom(default)
+    }
+  }
+).addTo(layer);
+
+var marker2 = new maptalks.Marker(
+  center.add(-0.01, 0),
+  {
+    'symbol' : {
+      'textName' : 'm5',
+      'textSize' : 14,
+      'markerFile'   : 'm5.png',
+      'markerHorizontalAlignment' : 'middle', // left, middle(default), right
+      'markerVerticalAlignment' : 'middle'    // top, middle, bottom(default)
+    }
+  }
+).addTo(layer);
