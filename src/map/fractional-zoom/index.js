0384c5d1cb28ec672f0467b53a8c1bc091582c3b
fuzhenn <fuzhen@maptalks.org>
2021-07-14 15:56:40 +0800

    add seamlessZoom, fix maptalks/maptalks#1386

--- src/map/fractional-zoom/index.js
+++ src/map/fractional-zoom/index.js
@@ -2,6 +2,7 @@ var map = new maptalks.Map('map', {
   center: [-0.113049,51.498568],
   zoom: 14.8,
   zoomControl : true,
+  seamlessZoom: true,
   baseLayer: new maptalks.TileLayer('base', {
     urlTemplate: '$(urlTemplate)',
     subdomains: $(subdomains),
