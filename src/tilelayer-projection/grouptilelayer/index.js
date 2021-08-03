aee87435e1644a31e38812ac4da41f5adeeef2da
hu de yi <mehudeyi@163.com>
2021-08-03 11:36:59 +0800

    fix some tilelayer error (#111)
    * fix some tilelayer error
    
    * update notes
    
    * update custom-tilelayer
    
    * update

--- src/tilelayer-projection/grouptilelayer/index.js
+++ src/tilelayer-projection/grouptilelayer/index.js
@@ -1,26 +1,21 @@
 var map = new maptalks.Map('map', {
-  center:     [-0.113049,51.498568],
-  zoom:  6,
-  pitch : 40,
+  center: [-0.113049, 51.498568],
+  zoom: 6,
+  pitch: 40,
   attribution: {
-    content: '$(attribution), &copy; BoudlessGeo'
+      content: ' &copy; carto'
   },
   // add 2 TileLayers with a GroupTileLayer
-  baseLayer : new maptalks.GroupTileLayer('base', [
-    new maptalks.TileLayer('tile2', {
-      urlTemplate: 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',
-      subdomains  : ['a','b','c','d']
-    }),
+  baseLayer: new maptalks.GroupTileLayer('base', [
+      new maptalks.TileLayer('tile2', {
+          urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c', 'd']
+      }),
 
-    new maptalks.WMSTileLayer('wms', {
-      'urlTemplate' : 'https://demo.boundlessgeo.com/geoserver/ows',
-      'crs' : 'EPSG:3857',
-      'layers' : 'ne:ne',
-      'styles' : '',
-      'version' : '1.3.0',
-      'format': 'image/png',
-      'transparent' : true,
-      'uppercase' : true
-    })
+      new maptalks.TileLayer('boudaries', {
+          'urlTemplate': 'https://{s}.basemaps.cartocdn.com/dark_only_labels/{z}/{x}/{y}.png',
+          'subdomains': ['a', 'b', 'c', 'd']
+      })
   ])
 });
+
