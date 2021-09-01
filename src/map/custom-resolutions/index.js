d69bc60295c6791630802cdc6713e2e89e4e620b
fuzhenn <fuzhen@maptalks.org>
2021-09-01 11:11:26 +0800

    add custom-resolutions example

---
+++ src/map/custom-resolutions/index.js
@@ -0,0 +1,33 @@
+var MAX_ZOOM = 28;
+var spatialReference = {
+    'projection': 'EPSG:3857',
+    'resolutions': (function () {
+        const resolutions = [];
+        const d = 2 * 6378137 * Math.PI;
+        for (let i = 0; i < MAX_ZOOM; i++) {
+            resolutions[i] = d / (256 * Math.pow(2, i));
+        }
+        return resolutions;
+    })(),
+    'fullExtent': {
+        'top': 6378137 * Math.PI,
+        'left': -6378137 * Math.PI,
+        'bottom': -6378137 * Math.PI,
+        'right': 6378137 * Math.PI
+    }
+};
+
+var map = new maptalks.Map('map', {
+  center: [-0.113049,51.498568],
+  zoom: 14,
+  spatialReference: spatialReference,
+  baseLayer: new maptalks.TileLayer('base', {
+    maxAvailableZoom: 20,
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains),
+    attribution: '$(attribution)'
+  }),
+  layers: [
+    new maptalks.VectorLayer('v')
+  ]
+});
