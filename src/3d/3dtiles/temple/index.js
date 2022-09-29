4a00e84f4985ac0226aa68963847714d29b5e5b0
yy923683900 <923683900@qq.com>
2022-09-29 16:13:35 +0800

    add some 3dtiles examples

---
+++ src/3d/3dtiles/temple/index.js
@@ -0,0 +1,28 @@
+const map = new maptalks.Map('map', {
+  center: [120.54632697304714, 27.593883052408984],
+  zoom: 18,
+  baseLayer: new maptalks.TileLayer('base', {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains),
+    attribution: '$(attribution)',
+  }),
+});
+
+const layer = new maptalks.Geo3DTilesLayer('3dtiles', {
+  services: [
+    {
+      url: 'http://data.mars3d.cn/3dtiles/qx-simiao/tileset.json',
+      ambientLight: [1, 1, 1],
+      maximumScreenSpaceError: 1,
+    },
+  ],
+});
+
+layer.once('loadtileset', (e) => {
+  const extent = layer.getExtent(e.index);
+  map.fitExtent(extent, 0, {
+    animation: false,
+  });
+});
+
+const groupLayer = new maptalks.GroupGLLayer('g', [layer]).addTo(map);
---
+++ src/vector/style/filter-data/index.js
@@ -0,0 +1,49 @@
+const map = new maptalks.Map('map', {
+  center: [-74.00912099912109, 40.71107610933129],
+  zoom: 16,
+  baseLayer: new maptalks.TileLayer('base', {
+    urlTemplate: '$(urlTemplate)',
+    subdomains: $(subdomains),
+    attribution: '$(attribution)',
+  }),
+});
+
+const vt = new maptalks.VectorTileLayer('vt', {
+  urlTemplate: 'http://tile.maptalks.com/test/planet-single/{z}/{x}/{y}.mvt',
+  spatialReference: 'preset-vt-3857',
+});
+
+const style = {
+  style: [
+    {
+      // Only data with building layer and Polygon type are displayed
+      // Please refer to the specific description of filter:http://doc.maptalks.com/docs/style/filter/feature-filter/
+      filter: ['all', ['==', '$layer', 'building'], ['==', '$type', 'Polygon']],
+      renderPlugin: {
+        dataConfig: {
+          type: 'fill',
+        },
+        type: 'fill',
+      },
+      symbol: {
+        polygonFill: '#577570',
+        polygonOpacity: 1,
+      },
+    },
+  ],
+};
+vt.setStyle(style);
+
+const sceneConfig = {
+  postProcess: {
+    enable: true,
+    antialias: {
+      enable: true,
+    },
+  },
+};
+
+const groupLayer = new maptalks.GroupGLLayer('group', [vt], {
+  sceneConfig,
+});
+groupLayer.addTo(map);
