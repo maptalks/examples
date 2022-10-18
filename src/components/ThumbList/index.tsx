468f802a3ea575a5c8100da984e996827264c5ed
yy923683900 <923683900@qq.com>
2022-10-18 17:51:12 +0800

    filter link thumblist

--- src/components/ThumbList/index.tsx
+++ src/components/ThumbList/index.tsx
@@ -9,41 +9,47 @@ function ThumbList() {
 
   return (
     <Container>
-      {examples[tabIndex].examples.map((exampleI) => (
-        <List key={exampleI.name}>
-          <Title>{exampleI.title[language]}</Title>
-          <Row gutter={[28, 24]}>
-            {exampleI.examples.map((exampleJ) => (
-              <StyledCol
-                hide={
-                  !exampleJ.title[language].includes(filter) ? "hide" : "show"
-                }
-                key={exampleJ.name}
-                span={6}
-              >
-                <Card
-                  onClick={() =>
-                    handleSelect(
-                      examples[tabIndex].name,
-                      exampleI.name,
-                      exampleJ.name
-                    )
-                  }
-                >
-                  <img
-                    width="100%"
-                    src={`/thumbnails/${examples[tabIndex].name}_${exampleI.name}_${exampleJ.name}.webp`}
-                  />
-                  <Text>
-                    <Circle />
-                    {exampleJ.title[language]}
-                  </Text>
-                </Card>
-              </StyledCol>
-            ))}
-          </Row>
-        </List>
-      ))}
+      {examples[tabIndex].examples
+        .filter((exampleI) => exampleI.name !== "plugin")
+        .map((exampleI) => (
+          <List key={exampleI.name}>
+            <Title>{exampleI.title[language]}</Title>
+            <Row gutter={[28, 24]}>
+              {exampleI.examples
+                .filter((exampleJ) => !exampleJ.url)
+                .map((exampleJ) => (
+                  <StyledCol
+                    hide={
+                      !exampleJ.title[language].includes(filter)
+                        ? "hide"
+                        : "show"
+                    }
+                    key={exampleJ.name}
+                    span={6}
+                  >
+                    <Card
+                      onClick={() =>
+                        handleSelect(
+                          examples[tabIndex].name,
+                          exampleI.name,
+                          exampleJ.name
+                        )
+                      }
+                    >
+                      <img
+                        width="100%"
+                        src={`/thumbnails/${examples[tabIndex].name}_${exampleI.name}_${exampleJ.name}.webp`}
+                      />
+                      <Text>
+                        <Circle />
+                        {exampleJ.title[language]}
+                      </Text>
+                    </Card>
+                  </StyledCol>
+                ))}
+            </Row>
+          </List>
+        ))}
     </Container>
   );
 }
