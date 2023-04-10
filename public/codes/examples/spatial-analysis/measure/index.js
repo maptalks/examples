e44c3e0ff57fd6a46c50ff3bee1f29673c405dc6
unknown <yy923683900>
2023-04-10 18:33:01 +0800

    update mt.gui

--- public/codes/examples/spatial-analysis/measure/index.js
+++ public/codes/examples/spatial-analysis/measure/index.js
@@ -23,68 +23,66 @@ const map = new maptalks.Map("map", {
   },
 });
 
-const layer = new maptalks.Geo3DTilesLayer('3dtiles', {        
-  services : [
-      {
-          url: 'http://resource.dvgis.cn/data/3dtiles/dayanta/tileset.json',
-          ambientLight : [1, 1, 1],
-          maximumScreenSpaceError: 1.0,
-          pointOpacity: 0.5,
-          pointSize: 3,
-          heightOffset: -400
-      }
-  ]
+const layer = new maptalks.Geo3DTilesLayer("3dtiles", {
+  services: [
+    {
+      url: "http://resource.dvgis.cn/data/3dtiles/dayanta/tileset.json",
+      ambientLight: [1, 1, 1],
+      maximumScreenSpaceError: 1.0,
+      pointOpacity: 0.5,
+      pointSize: 3,
+      heightOffset: -400,
+    },
+  ],
 });
 
-const groupGLLayer = new maptalks.GroupGLLayer("gl", [layer],
-  {
-    sceneConfig: {
-      environment: {
-        enable: true,
-        mode: 1,
-        level: 0,
-        brightness: 0.915,
-      },
-      ground: {
-        enable: true,
-        renderPlugin: {
-          type: "lit"
-        },
-        symbol: {
-          polygonOpacity: 1,
-          material: {
-            baseColorFactor: [0.48235, 0.48235, 0.48235, 1],
-            hsv: [0, 0, -0.532],
-            roughnessFactor: 0.22,
-            metallicFactor: 0.58,
-          }
-        }
-      }
+const groupGLLayer = new maptalks.GroupGLLayer("gl", [layer], {
+  sceneConfig: {
+    environment: {
+      enable: true,
+      mode: 1,
+      level: 0,
+      brightness: 0.915,
     },
-  }
-).addTo(map);
+    ground: {
+      enable: true,
+      renderPlugin: {
+        type: "lit",
+      },
+      symbol: {
+        polygonOpacity: 1,
+        material: {
+          baseColorFactor: [0.48235, 0.48235, 0.48235, 1],
+          hsv: [0, 0, -0.532],
+          roughnessFactor: 0.22,
+          metallicFactor: 0.58,
+        },
+      },
+    },
+  },
+}).addTo(map);
 
 let measureTool = null;
-layer.once('loadtileset', e => {
+layer.once("loadtileset", (e) => {
   const extent = layer.getExtent(e.index);
   map.fitExtent(extent, 0, { animation: false });
   measureTool = new maptalks.MeasureTool({
-    'language': 'zh-CN',
-    'metric': true,
-    'imperial': false,
-    'symbol': {
-      'lineColor': '#e8542b',
-      'dashLineColor': '#0f0',
-      'polygonFill': '#e8542b',
-      'polygonOpacity': 0.6
+    language: "zh-CN",
+    metric: true,
+    imperial: false,
+    symbol: {
+      lineColor: "#e8542b",
+      dashLineColor: "#0f0",
+      polygonFill: "#e8542b",
+      polygonOpacity: 0.6,
     },
-    'labelOptions': {
-      'boxStyle': {
-        'markerFile': '{res}/images/measure-tooltip.png',
-        'markerDx': 90,
-        'textDy': -20
-      } 
-    }
+    labelOptions: {
+      boxStyle: {
+        markerFile: "{res}/images/measure-tooltip.png",
+        markerDx: 90,
+        textDy: -20,
+      },
+    },
   }).addTo(groupGLLayer);
   measureTool.disable();
 });
@@ -94,12 +92,12 @@ gui
   .add({
     type: "button",
     label: "测量距离",
-    role: "play"
+    role: "distance",
   })
   .onClick(() => {
     if (measureTool) {
       measureTool.enable();
-      measureTool.setType('distance');
+      measureTool.setType("distance");
     }
   });
 
@@ -107,12 +105,12 @@ gui
   .add({
     type: "button",
     label: "测量面积",
-    role: "play"
+    role: "area",
   })
   .onClick(() => {
     if (measureTool) {
       measureTool.enable();
-      measureTool.setType('area');
+      measureTool.setType("area");
     }
   });
 
@@ -120,12 +118,12 @@ gui
   .add({
     type: "button",
     label: "测量高度",
-    role: "play"
+    role: "height",
   })
   .onClick(() => {
     if (measureTool) {
       measureTool.enable();
-      measureTool.setType('height');
+      measureTool.setType("height");
     }
   });
 
@@ -133,7 +131,7 @@ gui
   .add({
     type: "button",
     label: "清除全部",
-    role: "play"
+    role: "clear",
   })
   .onClick(() => {
     if (measureTool) {
