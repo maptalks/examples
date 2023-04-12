d155adae0711ad93a65187086e215c8fd225cecd
unknown <yy923683900>
2023-04-12 15:59:30 +0800

    add terrain examples

--- public/codes/examples/terrain/stylized/index.js
+++ public/codes/examples/terrain/stylized/index.js
@@ -1,25 +1,68 @@
 const map = new maptalks.Map("map", {
-  center: [-0.113049, 51.498568],
-  zoom: 14,
-  baseLayer: new maptalks.TileLayer("base", {
-    urlTemplate: "{urlTemplate}",
-    subdomains: ["a", "b", "c", "d"],
-    attribution: "{attribution}",
+  center: [114.31135353780905, 30.5437759669405, 46.900001525878906],
+  zoom: 18.20975314908587,
+  pitch: 0,
+  bearing: 0,
+});
+
+const targetCoord = new maptalks.Coordinate(0, 0);
+const POINT0 = new maptalks.Coordinate(0, 0);
+const POINT1 = new maptalks.Coordinate(0, 0);
+
+const skinLayers = [
+  new maptalks.TileLayer("base", {
+    maxAvailableZoom: 20,
+    spatialReference: {
+      projection: "EPSG:3857",
+    },
+    offset: function (z) {
+      const center = map.getCenter();
+      const c = maptalks.CRSTransform.transform(
+        center.toArray(),
+        "GCJ02",
+        "WGS84"
+      );
+      targetCoord.set(c[0], c[1]);
+      const offset = map
+        .coordToPoint(center, z, POINT0)
+        ._sub(map.coordToPoint(targetCoord, z, POINT1));
+      return offset._round().toArray();
+    },
+    urlTemplate:
+      "http://webst{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
+    subdomains: ["01", "02", "03", "04"],
+    attribution:
+      '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>',
   }),
-});
+];
 
-const gui = new dat.GUI({
-  // width: 250,
-});
+const group = new maptalks.GroupGLLayer("group", skinLayers);
+group.addTo(map);
 
-const Config = function () {
-  this.type = "卫星影像";
+/**start**/
+const terrain = {
+  type: "mapbox",
+  tileSize: 512,
+  spatialReference: "preset-vt-3857",
+  urlTemplate:
+    "https://{s}.tiles.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=pk.eyJ1IjoibWFwYm94LWdsLWpzIiwiYSI6ImNram9ybGI1ajExYjQyeGxlemppb2pwYjIifQ.LGy5UGNIsXUZdYMvfYRiAQ",
+  subdomains: ["a", "b", "c", "d"],
 };
-const options = new Config();
 
-const typeControl = gui
-  .add(options, "type", ["普通地形", "卫星影像"])
-  .name("");
-typeControl.onChange((value) => {
-  console.log(value);
-});
+const gui = new mt.GUI();
+
+gui
+  .add({
+    type: "select",
+    value: "satellite",
+    options: [
+      {
+        label: "卫星影像",
+        value: "satellite",
+      },
+    ],
+  })
+  .onChange((value) => {
+    console.log(value);
+  });
+/**end**/
--- public/codes/examples/terrain/tian/index.js
+++ public/codes/examples/terrain/tian/index.js
@@ -1,23 +1,67 @@
 const map = new maptalks.Map("map", {
-  center: [-0.113049, 51.498568],
-  zoom: 14,
-  baseLayer: new maptalks.TileLayer("base", {
-    urlTemplate: "{urlTemplate}",
-    subdomains: ["a", "b", "c", "d"],
-    attribution: "{attribution}",
+  center: [114.31135353780905, 30.5437759669405, 46.900001525878906],
+  zoom: 18.20975314908587,
+  pitch: 0,
+  bearing: 0,
+});
+
+const targetCoord = new maptalks.Coordinate(0, 0);
+const POINT0 = new maptalks.Coordinate(0, 0);
+const POINT1 = new maptalks.Coordinate(0, 0);
+
+const skinLayers = [
+  new maptalks.TileLayer("base", {
+    maxAvailableZoom: 20,
+    spatialReference: {
+      projection: "EPSG:3857",
+    },
+    offset: function (z) {
+      const center = map.getCenter();
+      const c = maptalks.CRSTransform.transform(
+        center.toArray(),
+        "GCJ02",
+        "WGS84"
+      );
+      targetCoord.set(c[0], c[1]);
+      const offset = map
+        .coordToPoint(center, z, POINT0)
+        ._sub(map.coordToPoint(targetCoord, z, POINT1));
+      return offset._round().toArray();
+    },
+    urlTemplate:
+      "http://webst{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
+    subdomains: ["01", "02", "03", "04"],
+    attribution:
+      '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>',
   }),
-});
+];
 
-const gui = new dat.GUI({
-  // width: 250,
-});
+const group = new maptalks.GroupGLLayer("group", skinLayers);
+group.addTo(map);
 
-const Config = function () {
-  this.enable = true;
+/**start**/
+const terrain = {
+  type: "tianditu",
+  tileSize: 512,
+  spatialReference: "preset-vt-3857",
+  urlTemplate:
+    "https://t{s}.tianditu.gov.cn/mapservice/swdx?T=elv_c&tk=648e42a59f95a6b9aae1d505d52d229f&x={x}&y={y}&l={z}",
+  subdomains: "01234567",
 };
-const options = new Config();
 
-const enableController = gui.add(options, "enable").name("显示地形");
-enableController.onChange((value) => {
-  console.log(value);
-});
+const gui = new mt.GUI();
+
+gui
+  .add({
+    type: "checkbox",
+    label: "显示地形",
+    value: false,
+  })
+  .onChange((value) => {
+    if (value) {
+      group.setTerrain(terrain);
+    } else {
+      group.setTerrain(null);
+    }
+  });
+/**end**/
