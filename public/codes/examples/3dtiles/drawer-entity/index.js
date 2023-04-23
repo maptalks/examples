7d421686c7bf9e8061b5bfe0c7ed154eda8aadee
liubgithub <471071362@qq.com>
2023-04-23 17:32:39 +0800

    模型抽屉效果

---
+++ public/codes/examples/3dtiles/drawer-entity/index.js
@@ -0,0 +1,165 @@
+/**start**/
+let map, selectedEntity = null, selectedMask = null;
+function loadEntityPolygon() {
+  fetch("{res}/geojson/house.json").then((data) => {
+    return data.json();
+  }).then((data) => {
+    const geometries = maptalks.GeoJSON.toGeometry(data, geo => {
+      geo.updateSymbol({
+        polygonOpacity: 0,
+        lineOpacity: 0
+      });
+    });
+    new maptalks.VectorLayer('vLayer', geometries).addTo(map);
+  });
+}
+
+fetch("{res}/msd/drawer-entity/map.json").then((data) => {
+  return data.json();
+}).then((data) => {
+  map = maptalks.Map.fromJSON("map", data);
+  const groupgllayer = map.getLayer('group');
+  const gltflayer = groupgllayer.getLayer('gltf0');
+  loadEntityPolygon();
+  map.on('click', e => {
+    const identifyData = groupgllayer.identify(e.coordinate)[0];
+    if (identifyData) {
+      const coordinate = new maptalks.Coordinate(identifyData.coordinate);
+      const height = coordinate.z;
+      const room = queryRoom(coordinate);
+      if (!room) {
+        return;
+      }
+      const properties = room.getProperties();
+      const { floorHeight, type } = properties;
+      const z = Math.floor(height / floorHeight) * floorHeight;
+      if (selectedEntity) {
+        selectedEntity.remove();
+      }
+      const symbol = getSymbol(type);
+      const offset = getOffset(type);
+      selectedEntity = new maptalks.GLTFMarker([offset.x, offset.y, z], {
+        symbol
+      }).addTo(gltflayer);
+      selectedEntity.outline();
+      // selectedMask = new maptalks.ClipInsideMask(room.getCoordinates(), {
+      //   heightRange: [z, z + floorHeight]
+      // });
+      // gltflayer.setMask(selectedMask);
+    }
+  });
+});
+
+function queryRoom(coordinate) {
+  const polygons = map.getLayer('vLayer').getGeometries();
+  for (let i = 0; i < polygons.length; i++) {
+    if (polygons[i].containsPoint(coordinate)) {
+      return polygons[i];
+    }
+  }
+  return null;
+}
+
+function getSymbol(type) {
+  return {
+    '1': {
+      url: '{res}/gltf/drawer-entity/1.gltf',
+      rotationZ: 20.54,
+      scaleX: 0.8
+    },
+    '2': {
+      url: '{res}/gltf/drawer-entity/2.gltf',
+      rotationZ: 20.54,
+      scaleX: 0.6571790551008414,
+      scaleY: 0.8214738188760521,
+      scaleZ: 0.8214738188760521
+    },
+    '3': {
+      url: '{res}/gltf/drawer-entity/3.gltf',
+      rotationZ: 20.54,
+      scaleX: 0.6571790551008414,
+      scaleY: 0.8214738188760521,
+      scaleZ: 0.8214738188760521
+    }
+  }[type];
+}
+
+function getOffset(type) {
+  return {
+    '1': {
+      x: 121.48425576988325,
+      y: 31.244894797942635
+    },
+    '2': {
+      x: 121.48533043050065,
+      y: 31.24436843761393
+    },
+    '3': {
+      x: 121.48386542031585,
+      y: 31.243875250404784
+    }
+  }[type];
+}
+/**end**/
+const gui = new mt.GUI();
+gui
+  .add({
+    type: "button",
+    label: "重置",
+    role: "clear",
+  })
+  .onClick(() => {
+    if (!selectedEntity) {
+      return;
+    }
+    selectedEntity.setCoordinates(positions[0]);
+    selectedEntity.cancelOutline();
+    selectedEntity.updateSymbol({'translationX': 0});
+    selectedEntity.updateSymbol({'translationY': 0});
+    selectedEntity.updateSymbol({'translationZ': 0});
+  });
+
+gui
+  .add({
+    type: "slider",
+    label: "X轴",
+    value: 0.0,
+    min: -100.0,
+    max: 100.0,
+    step: 0.1,
+  }).onChange(function (value) {
+    if (!selectedEntity) {
+      return;
+    }
+    selectedEntity.updateSymbol({'translationX': value});
+});
+
+gui
+  .add({
+    type: "slider",
+    label: "Y轴",
+    value: 0.0,
+    min: -100.0,
+    max: 100.0,
+    step: 0.1,
+  }).onChange(function (value) {
+    if (!selectedEntity) {
+      return;
+    }
+    selectedEntity.updateSymbol({'translationY': value});
+});
+
+gui
+  .add({
+    type: "slider",
+    label: "Z轴",
+    value: 0.0,
+    min: -100.0,
+    max: 100.0,
+    step: 0.1,
+  }).onChange(function (value) {
+    if (!selectedEntity) {
+      return;
+    }
+    selectedEntity.updateSymbol({'translationZ': value});
+});
--- public/codes/examples/3dtiles/view-monomer/index.js
+++ public/codes/examples/3dtiles/view-monomer/index.js
@@ -1,86 +1,231 @@
 const map = new maptalks.Map("map", {
-  center: [108.95965, 34.2189],
-  zoom: 18,
-  bearing: 0,
-  pitch: 45,
+  center: [-73.88756247170068, 40.68791104561976],
+  zoom: 17.1,
+  bearing: 168.1,
+  pitch: 71.2,
   lights: {
-    directional: { direction: [-1, -1, -1], color: [1, 1, 1] },
+    directional: {
+      direction: [0.5, 0, -1],
+      color: [1, 1, 1],
+    },
     ambient: {
       resource: {
-        url: {
-          front: "{res}/hdr/923/front.jpg",
-          back: "{res}/hdr/923/back.jpg",
-          left: "{res}/hdr/923/left.jpg",
-          right: "{res}/hdr/923/right.jpg",
-          top: "{res}/hdr/923/top.jpg",
-          bottom: "{res}/hdr/923/bottom.jpg",
-        },
+        url: "{res}/hdr/photo_studio_01_1k.hdr",
+        sh: [
+          0.35916795154960834, 0.37332532618710934, 0.3749743179025387,
+          0.001946880750939448, 0.002781113205316295, 0.0038630691377190348,
+          0.009650859029741232, 0.010718360804091567, 0.011421607564558255,
+          0.024126928868387097, 0.024543636739605402, 0.023797007154584764,
+          -0.004899031092214359, -0.0048242535892225324, -0.005206229725454972,
+          -0.005589141785593036, -0.005507435440934302, -0.005472906061627375,
+          -0.009765483272152252, -0.010407014201306911, -0.009769691118007934,
+          -0.081841972255034, -0.0852762331787664, -0.0861865957833508,
+          -0.005844864502254523, -0.006733772922993404, -0.005910269638953521,
+        ],
       },
-      exposure: 1.426,
+      exposure: 0.787,
       hsv: [0, 0, 0],
-      orientation: 302.553,
-    }
-  }
+      orientation: 0,
+    },
+  },
 });
 
-const layer = new maptalks.Geo3DTilesLayer("3dtiles", {
-  services: [
-    {
-      url: "http://examples.maptalks.com/samples/ifc/test1/tileset.json",
-      maximumScreenSpaceError: 8.0,
-      heightOffset: -40,
-      opacity: 0.1
-    }
-  ]
-});
-layer.once('loadtileset', e => {
-  const extent = layer.getExtent(e.index);
-  map.fitExtent(extent, 0, { animation: false });
-});
-
-const groupGLLayer = new maptalks.GroupGLLayer("gl", [layer], {
-  sceneConfig: {
-    environment: {
-      enable: true,
-      mode: 1,
-      level: 0,
-      brightness: 0.915,
-    },
-    postProcess: {
-      enable: true
-    },
-    ground: {
-      enable: true,
-      renderPlugin: {
-        type: "lit",
+const vtStyle = [
+  {
+    filter: [
+      "all",
+      ["==", "$layer", "entertainment"],
+      ["==", "$type", "Polygon"],
+    ],
+    renderPlugin: {
+      dataConfig: {
+        type: "fill",
       },
-      symbol: {
-        polygonOpacity: 1,
-        material: {
-          baseColorFactor: [0.48235, 0.48235, 0.48235, 1],
-          hsv: [0, 0, -0.532],
-          roughnessFactor: 0.22,
-          metallicFactor: 0.58,
-        }
+      sceneConfig: {},
+      type: "fill",
+    },
+    symbol: {
+      polygonBloom: false,
+      polygonFill: [
+        0.5725490196078431, 0.6980392156862745, 0.5450980392156862, 1,
+      ],
+      polygonOpacity: 1,
+      polygonPatternFile: null,
+    },
+  },
+  {
+    filter: [
+      "all",
+      ["==", "$layer", "entertainment"],
+      ["==", "$type", "Polygon"],
+    ],
+    renderPlugin: {
+      dataConfig: {
+        type: "line",
+      },
+      sceneConfig: {},
+      type: "line",
+    },
+    symbol: {
+      lineColor: [0.73, 0.73, 0.73, 1],
+      lineWidth: 2
+    },
+  },
+  {
+    filter: ["all", ["==", "$layer", "building"], ["==", "$type", "Polygon"]],
+    renderPlugin: {
+      type: "lit",
+      dataConfig: {
+        type: "3d-extrusion",
+        altitudeProperty: "height",
+        minHeightProperty: "min_height",
+        altitudeScale: 1,
+        defaultAltitude: 10,
+        topThickness: 0,
+        top: true,
+        side: true,
       }
-    }
+    },
+    symbol: {
+      material: {
+        baseColorFactor: [1, 1, 1, 1],
+        hsv: [0, 0, -0.32],
+        baseColorIntensity: 1.532,
+        roughnessFactor: 1,
+        metallicFactor: 0,
+        emissiveTexture: "{res}/textures/897/1.jpg",
+        emissiveFactor: [
+          0.9333333333333333, 0.9254901960784314, 0.9607843137254902,
+        ],
+        emitColorFactor: 0.31
+      }
+    },
+  },
+  {
+    filter: [
+      "all",
+      ["==", "$layer", "secondary-road"],
+      ["==", "$type", "LineString"],
+    ],
+    renderPlugin: {
+      dataConfig: {
+        type: "line",
+      },
+      sceneConfig: {},
+      type: "line",
+    },
+    symbol: {
+      lineColor: [1, 1, 1, 1],
+      linePatternFile: "{res}/patterns/d4b.jpg",
+      lineWidth: {
+        type: "exponential",
+        default: 2,
+        stops: [
+          [14, 2],
+          [15, 4],
+          [16, 10],
+          [17, 20],
+          [18, 50],
+          [20.7, 100],
+          [22, 200],
+        ],
+      },
+    },
+  },
+];
+
+const vtLayer = new maptalks.VectorTileLayer("vt", {
+  urlTemplate: "http://tile.maptalks.com/test/planet-single/{z}/{x}/{y}.mvt",
+  spatialReference: "preset-vt-3857",
+  style: vtStyle,
+});
+
+const gltfLayer = new maptalks.GLTFLayer("gltf");
+
+const houseMarker = new maptalks.GLTFMarker(
+  [-73.88713688860861, 40.68848442450471],
+  {
+    id: 'house',
+    symbol: {
+      shadow: true,
+      url: "{res}/gltf/29c/scene.gltf",
+      scaleX: 8.12466,
+      scaleY: 8.12466,
+      scaleZ: 8.12466,
+      rotationZ: 299.6285,
+      uniforms: {
+        polygonFill: [1, 1, 1, 1],
+        polygonOpacity: 1
+      }
+    },
+    zoomOnAdded: 17,
   }
-}).addTo(map);
+).addTo(gltfLayer);
 
 /**start**/
-let lastHit = null;
-map.on('click', e => {
-  if (maptalks.Util.isNumber(lastHit)) {
-      layer.cancelHighlight(0, [lastHit]);
+const position = [-73.88468305785034, 40.68931616877157, 34.78572];
+const roomMarker = new maptalks.GLTFMarker(position,
+  {
+    symbol: {
+      shadow: false,
+      url: "{res}/gltf/room/room.gltf",
+      scaleX: 1.1719917308622843,
+      scaleY: 1.1719917308622843,
+      scaleZ: 1.1719917308622843,
+      rotationZ: 299.6285 + 180,
+      shader: "pbr",
+      uniforms: {
+        polygonFill: [1, 1, 1, 1],
+        polygonOpacity: 1,
+        baseColorIntensity: 1,
+        outputSRGB: 1,
+      }
+    },
+    zoomOnAdded: 17,
   }
-  const hits = layer.identify(e.coordinate);
-  if (hits.length) {
-    layer.highlight({
-        id: hits[0].data.batchId,
-        opacity: 0.999
-    });
-    lastHit = hits[0].data.batchId;
+).addTo(gltfLayer);
+
+const sceneConfig = {
+  environment: {
+    enable: true,
+    mode: 1,
+    level: 0,
+    brightness: 0.489,
+  },
+  shadow: {
+    type: "esm",
+    enable: true,
+    quality: "high",
+    opacity: 0.5,
+    color: [0, 0, 0],
+    blurOffset: 1,
+  },
+  postProcess: {
+    enable: true
+  },
+  ground: {
+    enable: true,
+    renderPlugin: {
+      type: "fill",
+    },
+    symbol: {
+      polygonFill: [0.803921568627451, 0.803921568627451, 0.803921568627451, 1],
+      polygonOpacity: 1,
+    },
+  },
+};
+
+const groupGLLayer = new maptalks.GroupGLLayer("gl", [vtLayer, gltfLayer], {
+  sceneConfig,
+}).addTo(map);
+
+map.on('click', e => {
+  const identifyData = groupGLLayer.identify(e.coordinate)[0];
+  const picked = identifyData && identifyData.data;
+  if (picked && picked.getId() !== 'house') {
+    houseMarker.setUniform('polygonOpacity', 0.1);
+  } else {
+    houseMarker.setUniform('polygonOpacity', 1);
   }
 });
 /**end**/
-
