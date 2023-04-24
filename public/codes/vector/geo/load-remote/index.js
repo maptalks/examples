568f15099883211179eb1cce2d68f1c45542dfa4
unknown <yy923683900>
2023-04-24 14:33:37 +0800

    修改GeoJSONVectorTileLayer示例底图

--- public/codes/vector/geo/load-remote/index.js
+++ public/codes/vector/geo/load-remote/index.js
@@ -1,38 +1,56 @@
 const map = new maptalks.Map("map", {
   center: [-74.00912099912109, 40.71107610933129],
   zoom: 15,
-  baseLayer: new maptalks.TileLayer("base", {
-    urlTemplate: "{urlTemplate}",
-    subdomains: ["a", "b", "c", "d"],
-    attribution: "{attribution}",
-  }),
+  bearing: 157,
+  pitch: 80,
+  lights: {
+    directional: {
+      direction: [1, 0, -1],
+      color: [1, 1, 1],
+    },
+    ambient: {
+      resource: {
+        url: {
+          front: "{res}/hdr/gradient/front.png",
+          back: "{res}/hdr/gradient/back.png",
+          left: "{res}/hdr/gradient/left.png",
+          right: "{res}/hdr/gradient/right.png",
+          top: "{res}/hdr/gradient/top.png",
+          bottom: "{res}/hdr/gradient/bottom.png",
+        },
+        prefilterCubeSize: 1024,
+      },
+      exposure: 1,
+      hsv: [0, 0.34, 0],
+      orientation: 0,
+    },
+  },
 });
 
-const style = {
-  style: [
-    {
-      filter: true,
-      renderPlugin: {
-        dataConfig: {
-          type: "point",
-        },
-        sceneConfig: {
-          collision: true,
-          fading: false,
-          depthFunc: "always",
-        },
-        type: "icon",
+/**start**/
+const style = [
+  {
+    filter: true,
+    renderPlugin: {
+      dataConfig: {
+        type: "point",
       },
-      symbol: {
-        markerType: "ellipse",
-        markerFill: "#1bbc9b",
-        markerFillOpacity: 1,
-        markerHeight: 21,
-        markerWidth: 21,
+      sceneConfig: {
+        collision: true,
+        fading: false,
+        depthFunc: "always",
       },
+      type: "icon",
     },
-  ],
-};
+    symbol: {
+      markerType: "ellipse",
+      markerFill: "#1bbc9b",
+      markerFillOpacity: 1,
+      markerHeight: 21,
+      markerWidth: 21,
+    },
+  },
+];
 
 const layer = new maptalks.GeoJSONVectorTileLayer("geo", {
   data: "{res}/geojson/area.geojson",
@@ -42,6 +60,16 @@ const layer = new maptalks.GeoJSONVectorTileLayer("geo", {
 layer.on("dataload", (e) => {
   map.fitExtent(e.extent);
 });
+/**end**/
 
-const groupLayer = new maptalks.GroupGLLayer("group", [layer]);
+const groupLayer = new maptalks.GroupGLLayer("group", [layer], {
+  sceneConfig: {
+    environment: {
+      enable: true,
+      mode: 1,
+      level: 0,
+      brightness: 0,
+    },
+  },
+});
 groupLayer.addTo(map);
--- public/codes/vector/geo/transform-function/index.js
+++ public/codes/vector/geo/transform-function/index.js
@@ -1,13 +1,33 @@
 const map = new maptalks.Map("map", {
   center: [-74.00912099912109, 40.71107610933129],
   zoom: 16,
-  baseLayer: new maptalks.TileLayer("base", {
-    urlTemplate: "{urlTemplate}",
-    subdomains: ["a", "b", "c", "d"],
-    attribution: "{attribution}",
-  }),
+  bearing: 157,
+  pitch: 80,
+  lights: {
+    directional: {
+      direction: [1, 0, -1],
+      color: [1, 1, 1],
+    },
+    ambient: {
+      resource: {
+        url: {
+          front: "{res}/hdr/gradient/front.png",
+          back: "{res}/hdr/gradient/back.png",
+          left: "{res}/hdr/gradient/left.png",
+          right: "{res}/hdr/gradient/right.png",
+          top: "{res}/hdr/gradient/top.png",
+          bottom: "{res}/hdr/gradient/bottom.png",
+        },
+        prefilterCubeSize: 1024,
+      },
+      exposure: 1,
+      hsv: [0, 0.34, 0],
+      orientation: 0,
+    },
+  },
 });
 
+/**start**/
 const data = {
   properties: {
     adcode: "110000",
@@ -49,31 +69,39 @@ layer.on("dataload", (e) => {
   map.fitExtent(e.extent);
 });
 
-layer.setStyle({
-  style: [
-    {
-      filter: true,
-      renderPlugin: {
-        dataConfig: {
-          type: "point",
-        },
-        sceneConfig: {
-          collision: true,
-          fading: false,
-          depthFunc: "always",
-        },
-        type: "icon",
+layer.setStyle([
+  {
+    filter: true,
+    renderPlugin: {
+      dataConfig: {
+        type: "point",
       },
-      symbol: {
-        markerType: "ellipse",
-        markerFill: "#1bbc9b",
-        markerFillOpacity: 1,
-        markerHeight: 21,
-        markerWidth: 21,
+      sceneConfig: {
+        collision: true,
+        fading: false,
+        depthFunc: "always",
       },
+      type: "icon",
     },
-  ],
-});
+    symbol: {
+      markerType: "ellipse",
+      markerFill: "#1bbc9b",
+      markerFillOpacity: 1,
+      markerHeight: 21,
+      markerWidth: 21,
+    },
+  },
+]);
+/**end**/
 
-const groupLayer = new maptalks.GroupGLLayer("group", [layer]);
+const groupLayer = new maptalks.GroupGLLayer("group", [layer], {
+  sceneConfig: {
+    environment: {
+      enable: true,
+      mode: 1,
+      level: 0,
+      brightness: 0,
+    },
+  },
+});
 groupLayer.addTo(map);
