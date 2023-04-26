2d3fbc046a5f2255b07aa51d72a4f665cf99ee4c
unknown <yy923683900>
2023-04-26 16:10:43 +0800

    优化面数据样式示例

--- public/codes/gltf/gltf-marker/shader/index.js
+++ public/codes/gltf/gltf-marker/shader/index.js
@@ -1,57 +1,105 @@
 const map = new maptalks.Map("map", {
-  center: [-0.10707916972842213, 51.48119259984284],
-  zoom: 12,
-  pitch: 63.8,
-  bearing: 179.39999999999975,
-  baseLayer: new maptalks.TileLayer("base", {
-    urlTemplate: "{urlTemplate}",
-    subdomains: ["a", "b", "c", "d"],
-    attribution: "{attribution}",
-  }),
+  center: [-74.01252272617671, 40.70709931736744],
+  zoom: 16,
+  pitch: 60,
   lights: {
     ambient: {
       resource: {
-        url: "{res}/hdr/env.hdr",
+        url: {
+          front: "{res}/hdr/gradient/front.png",
+          back: "{res}/hdr/gradient/back.png",
+          left: "{res}/hdr/gradient/left.png",
+          right: "{res}/hdr/gradient/right.png",
+          top: "{res}/hdr/gradient/top.png",
+          bottom: "{res}/hdr/gradient/bottom.png",
+        },
+        prefilterCubeSize: 1024,
       },
-      color: [1, 1, 1],
       exposure: 1,
+      hsv: [0, 1, -0.042],
+      orientation: 0,
     },
     directional: {
+      direction: [-0.1, 1, -1],
       color: [1, 1, 1],
-      lightColorIntensity: 5000,
-      direction: [1, -0.4, -1],
     },
   },
 });
 
-const gui = new dat.GUI({
-  width: 250,
-});
-class Config {
-  constructor() {
-    this.shaderList = "pbr";
-  }
-}
-const options = new Config();
+/**start**/
 const url = "{res}/gltf/alien/alien.glb";
 const symbol = {
-  shader: options.shaderList,
   url: url,
-  scale: [1.5, 1.5, 1.5],
+  shader: "pbr",
+  rotationZ: 180,
+  scaleX: 1.5,
+  scaleY: 1.5,
+  scaleZ: 1.5,
 };
 
-const gltfLayer = new maptalks.GLTFLayer("gltf");
-const gltfMarker = new maptalks.GLTFMarker(
-  [-0.11304900000004636, 51.498568000000006],
-  {
-    symbol: symbol,
-  }
-).addTo(gltfLayer);
+const layer = new maptalks.GLTFLayer("gltf");
+const marker = new maptalks.GLTFMarker(map.getCenter(), {
+  symbol: symbol,
+}).addTo(layer);
 
-const groupGLLayer = new maptalks.GroupGLLayer("gl", [gltfLayer]).addTo(map);
-const shaderListControl = gui
-  .add(options, "shaderList", ["pbr", "phong", "wireframe"])
-  .name("shader list");
-shaderListControl.onChange(function (value) {
-  gltfMarker.setShader(value);
-});
+function setShader(value) {
+  marker.setShader(value);
+}
+/**end**/
+
+const groupLayer = new maptalks.GroupGLLayer("group", [layer], {
+  sceneConfig: {
+    environment: {
+      enable: true,
+      mode: 1,
+      level: 0,
+      brightness: 0,
+    },
+    ground: {
+      enable: true,
+      renderPlugin: {
+        type: "lit",
+      },
+      symbol: {
+        polygonFill: [0.54, 0.54, 0.54, 1],
+        ssr: true,
+        material: {
+          baseColorTexture: "{res}/textures/rubber_roughness.png",
+          baseColorFactor: [0.3450981, 0.3372549, 0.2117647, 1],
+          hsv: [-0.468, 0, -0.128],
+          baseColorIntensity: 1.372,
+          contrast: 1.372,
+          roughnessFactor: 1,
+          metallicFactor: 0,
+          normalTexture: "{res}/textures/rubber_roughness.png",
+          uvScale: [0.09, 0.09],
+          normalMapFactor: 0.68,
+          emitColorFactor: 1.11,
+          noiseTexture: "{res}/textures/noise.png",
+        },
+      },
+    },
+  },
+}).addTo(map);
+
+const gui = new mt.GUI();
+
+gui
+  .add({
+    label: "着色器",
+    type: "select",
+    value: "pbr",
+    options: [
+      {
+        label: "pbr",
+        value: "pbr",
+      },
+      {
+        label: "phong",
+        value: "phong",
+      },
+    ],
+  })
+  .onChange((value) => {
+    setShader(value);
+  });
